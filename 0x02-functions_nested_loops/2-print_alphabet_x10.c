bdb71d467986359d8e6636f68dba2164e286f546
betty-27 <betelhemtilahunm@gmail.com>
2022-09-13 21:27:24 +0300

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+int alphabet;
+int counter;
+counter = 0;
+while (counter < 10)
+{
+for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+counter++;
+_putchar('\n');
+}
+}
